2a9ae8f973bfa5b33c5c0a6859bff529c5d57299
Francis Dominic Fajardo <fajardo@duck.com>
2025-03-08 16:23:22 +0800

    Make project list images use browser-level lazy-loading

--- assets/main.js
+++ assets/main.js
@@ -268,6 +268,7 @@ var gSite = {
                 previewImage.height = "200";
                 previewImage.src = `${baseSourceUrl}.jpg`;
                 previewImage.classList.add("loading");
+                previewImage.loading = "lazy";
                 previewImage.addEventListener("load", async function () {
                     previewImage.classList.remove("loading");
                     previewPlaceholder.classList.remove("phs");
